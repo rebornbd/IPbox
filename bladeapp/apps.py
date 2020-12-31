01dfa2413a7414767685136fda5e733b988d21de
rebornbd <d.dipta.cse@gmail.com>
2020-12-31 23:49:43 +0600

    ipbox init + create apps

---
+++ bladeapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BladeappConfig(AppConfig):
+    name = 'bladeapp'
---
+++ clusterapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ClusterappConfig(AppConfig):
+    name = 'clusterapp'
