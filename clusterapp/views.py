725c6dbca28478171a6bce6327e91085e396dacd
rebornbd <dipta.dhar.bd@gmail.com>
2021-07-18 18:43:41 +0600

    permission update | Cluster

--- clusterapp/views.py
+++ clusterapp/views.py
@@ -1,5 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 from django.core.paginator import Paginator
+from django.urls import reverse
+
+from accounts.views import generalUser, adminUser
 from slotapp.models import Slot
 from vmapp.models import VM
 from .models import Cluster
@@ -15,11 +20,17 @@ def clusterList(request):
 
   context = {
     'page_obj': page_obj,
+    'generalUser': generalUser(request.user),
+    'adminUser': adminUser(request.user)
   }
   return render(request, 'clusterapp/clusters.html', context)
 
 
+@login_required(login_url='accountsapp:login')
 def clusterCreate(request):
+  if not adminUser(request.user):
+    return HttpResponseRedirect(reverse('homepage'))
+  
   if (request.POST):
     name      = request.POST['clustername']
     ip        = request.POST['clusterip']
@@ -33,9 +44,12 @@ def clusterCreate(request):
   return render(request, 'clusterapp/cluster_create.html')
 
 
+@login_required(login_url='accountsapp:login')
 def clusterUpdate(request, id):
+  if not adminUser(request.user):
+    return HttpResponseRedirect(reverse('homepage'))
+
   cluster = Cluster.objects.get(pk=id)
-    
   if (request.POST):
     name      = request.POST['clustername']
     ip        = request.POST['clusterip']
