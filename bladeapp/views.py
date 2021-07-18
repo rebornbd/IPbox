92ca166834b82ce94bd3b7b0587e738b5eb961d4
rebornbd <dipta.dhar.bd@gmail.com>
2021-07-18 18:22:52 +0600

    update user permission

--- bladeapp/views.py
+++ bladeapp/views.py
@@ -1,5 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 from django.core.paginator import Paginator
+from django.urls import reverse
+
+from accounts.views import generalUser, adminUser
 from siteapp.models import Site
 from rackapp.models import Rack
 from .models import Blade
@@ -15,11 +20,17 @@ def bladeList(request):
 
   context = {
     'page_obj': page_obj,
+    'generalUser': generalUser(request.user),
+    'adminUser': adminUser(request.user)
   }
   return render(request, 'bladeapp/blades.html', context)
 
 
+@login_required(login_url='accountsapp:login')
 def bladeCreate(request):
+  if not adminUser(request.user):
+    return HttpResponseRedirect(reverse('homepage'))
+  
   if (request.POST):
     name    = request.POST['bladename']
     ip      = request.POST['bladeip']
@@ -37,9 +48,12 @@ def bladeCreate(request):
   return render(request, 'bladeapp/blade_create.html', context=context)
 
 
+@login_required(login_url='accountsapp:login')
 def bladeUpdate(request, id):
+  if not adminUser(request.user):
+    return HttpResponseRedirect(reverse('homepage'))
+  
   blade = Blade.objects.get(pk=id)
-    
   if (request.POST):
     name    = request.POST['bladename']
     ip      = request.POST['bladeip']
@@ -60,6 +74,7 @@ def bladeUpdate(request, id):
   return render(request, 'bladeapp/blade_update.html', context=context)
 
 
+@login_required(login_url='accountsapp:login')
 def bladeQuery(request, id):
   blade = Blade.objects.get(pk=id)
 
