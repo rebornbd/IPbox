e70cd21be221bd70a3d3c8d1e92dd10468cd28fd
rebornbd <dipta.dhar.bd@gmail.com>
2021-07-18 18:31:57 +0600

    slot permission update

--- slotapp/views.py
+++ slotapp/views.py
@@ -1,5 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 from django.core.paginator import Paginator
+from django.urls import reverse
+
+from accounts.views import generalUser, adminUser
 from bladeapp.models import Blade
 from .models import Slot
 
@@ -14,11 +19,17 @@ def slotList(request):
 
   context = {
     'page_obj': page_obj,
+    'generalUser': generalUser(request.user),
+    'adminUser': adminUser(request.user)
   }
   return render(request, 'slotapp/slots.html', context)
 
 
+@login_required(login_url='accountsapp:login')
 def slotCreate(request):
+  if not adminUser(request.user):
+    return HttpResponseRedirect(reverse('homepage'))
+  
   if (request.POST):
     name    = request.POST['slotname']
     ip      = request.POST['slotip']
@@ -36,9 +47,12 @@ def slotCreate(request):
   return render(request, 'slotapp/slot_create.html', context=context)
 
 
+@login_required(login_url='accountsapp:login')
 def slotUpdate(request, id):
+  if not adminUser(request.user):
+    return HttpResponseRedirect(reverse('homepage'))
+  
   slot = Slot.objects.get(pk=id)
-    
   if (request.POST):
     name    = request.POST['slotname']
     ip      = request.POST['slotip']
@@ -59,6 +73,7 @@ def slotUpdate(request, id):
   return render(request, 'slotapp/slot_update.html', context=context)
 
 
+@login_required(login_url='accountsapp:login')
 def slotQuery(request, id):
   slot = Slot.objects.get(pk=id)
 
